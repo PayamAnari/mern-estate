e66f1b42f806d1975137f2300967ff2408f12e2e
PayamAnari <anari.p62@gmail.com>
2023-11-30 12:04:47 +0100

    Create signup route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose
@@ -13,7 +15,11 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
