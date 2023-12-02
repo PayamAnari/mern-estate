d8cad5413b2e1f719d21e58d6efd473ec4f3d7c0
PayamAnari <anari.p62@gmail.com>
2023-12-02 12:45:06 +0100

    Add facebook login functionallity

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,13 +1,13 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth';
+import { GoogleAuthProvider, FacebookAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import { app } from '../firebase';
-import { useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { signInSuccess } from '../redux/user/userSlice';
 import { useNavigate } from 'react-router-dom';
-
 
 export default function OAuth() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
@@ -15,25 +15,49 @@ export default function OAuth() {
 
       const result = await signInWithPopup(auth, provider);
 
-      const res = await fetch ('/api/auth/google', {
+      const res = await fetch('/api/auth/google', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ name: result.user.displayName, email: result.user.email, photo: result.user.photoURL}),
-      })
-      const  data = await res.json();
+        body: JSON.stringify({ name: result.user.displayName, email: result.user.email, photo: result.user.photoURL }),
+      });
+
+      const data = await res.json();
       dispatch(signInSuccess(data));
       navigate('/');
-
     } catch (error) {
-      console.log('Could not sign in with Google', error)
+      console.log('Could not sign in with Google', error);
     }
-  }
+  };
 
+  const handleFacebookClick = async () => {
+    try {
+      const provider = new FacebookAuthProvider();
+      const auth = getAuth(app);
 
+      const result = await signInWithPopup(auth, provider);
+
+      const res = await fetch('/api/auth/facebook', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: result.user.displayName, email: result.user.email, photo: result.user.photoURL }),
+      });
+
+      const data = await res.json();
+      dispatch(signInSuccess(data));
+      navigate('/');
+    } catch (error) {
+      console.log('Could not sign in with Facebook', error);
+    }
+  };
 
   return (
-    <button onClick={handleGoogleClick} type='button' className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-75'>Continue with google</button>
-  )
+    <div className='flex flex-col gap-4 '>
+      <button onClick={handleGoogleClick} type='button' className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-75'>Continue with Google</button>
+      <button onClick={handleFacebookClick} type='button' className='bg-blue-700 text-white p-3 rounded-lg uppercase hover:opacity-75'>Continue with Facebook</button>
+    </div>
+  );
 }
