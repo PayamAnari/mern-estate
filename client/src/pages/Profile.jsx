89db174e1fd62fcc3e9e3ad15ee8d965fe69180e
PayamAnari <anari.p62@gmail.com>
2023-12-06 10:08:57 +0100

    Add delete btn to show listings

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -15,6 +15,7 @@ export default function Profile() {
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
   const [showListingError, setShowListingError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -121,8 +122,29 @@ export default function Profile() {
         setShowListingError(true);
         return;
       }
+      setUserListings(data);
+
     } catch (error) {
       setShowListingError(true);
+    }
+  }
+
+  const handleListingDelete = async () => {
+    try {
+      const res = await fetch(`/api/user/listings/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.jsn();
+      if(data.success === false){
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) => 
+      prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
     }
   }
 
@@ -162,6 +184,47 @@ export default function Profile() {
       <p className='text-green-700 mt-5'>{updateSuccess ? 'User is updated successfully!' : ''}</p>
       <button disabled={loading} onClick={handleShowListings} className='bg-yellow-500 text-white p-3 rounded-lg uppercase hover:opacity-95 w-full'>{loading ? 'Show Listings...' : 'Show listings'}</button>
       <p className='text-red-700 mt-5'>{showListingError ? 'Error showing listings' : ''}</p>
+     
+      {userListings && userListings.length > 0 && (
+        <div className='flex flex-col gap-4'>
+          <h1 className='text-center mt-7 text-2xl font-semibold'>
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className='border rounded-lg p-3 flex justify-between items-center gap-4'
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt='listing cover'
+                  className='h-16 w-16 object-contain'
+                />
+              </Link>
+              <Link
+                className='text-slate-700 font-semibold  hover:underline truncate flex-1'
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+
+              <div className='flex flex-col item-center'>
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className='text-red-700 uppercase'
+                >
+                  Delete
+                </button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className='text-green-700 uppercase'>Edit</button>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+     
     </div>
   )
 }
