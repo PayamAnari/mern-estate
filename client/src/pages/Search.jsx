53391f3c7fbd1602fc3c18248363fd3501837e57
PayamAnari <anari.p62@gmail.com>
2023-12-10 22:05:04 +0100

    Create listing item component

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import ListingItem from '../components/ListingItem';
 
 export default function Search() {
   const navigate = useNavigate();
@@ -242,7 +243,11 @@ export default function Search() {
             </p>
           )}
 
-    
+          {!loading &&
+            listings &&
+            listings.map((listing) => (
+              <ListingItem key={listing._id} listing={listing} />
+            ))}
 
           {showMore && (
             <button
