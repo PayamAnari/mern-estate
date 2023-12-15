58ae86383f2a8e24a313723275f3ea76ea542095
PayamAnari <anari.p62@gmail.com>
2023-12-15 17:14:09 +0100

    Update contact page

--- client/src/pages/ContactInfo.jsx
+++ client/src/pages/ContactInfo.jsx
@@ -1,7 +1,7 @@
 export default function ContactInfo() {
   return (
     <section className='relative z-10 overflow-hidden bg-white dark:bg-dark py-20 lg:py-[120px]'>
-      <div className='container mx-auto'>
+      <div className='container mr-4 ml-3 sm:mx-auto sm:ml-4'>
         <div className='flex flex-wrap -mx-4 lg:justify-between'>
           <div className='w-full px-4 lg:w-1/2 xl:w-6/12'>
             <div className='mb-12 max-w-[570px] lg:mb-0'>
@@ -9,9 +9,9 @@ export default function ContactInfo() {
                 Contact Us
               </span>
               <h2 className='text-dark dark:text-white mb-6 text-[32px] font-bold uppercase sm:text-[40px] lg:text-[36px] xl:text-[40px]'>
-                GET IN TOUCH WITH US
+                GET IN <span className='text-blue-600'>TOUCH</span> WITH US
               </h2>
-              <p className='text-base leading-relaxed text-body-color dark:text-dark-6 mb-9'>
+              <p className='text-base leading-relaxed text-body-color dark:text-dark-6 mb-9 mr-4'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eius tempor incididunt ut labore et dolore magna aliqua. Ut enim
                 adiqua minim veniam quis nostrud exercitation ullamco
@@ -24,6 +24,7 @@ export default function ContactInfo() {
                     viewBox='0 0 32 32'
                     fill='none'
                     xmlns='http://www.w3.org/2000/svg'
+                    className='text-blue-700'
                   >
                     <path
                       d='M30.6 11.8002L17.7 3.5002C16.65 2.8502 15.3 2.8502 14.3 3.5002L1.39998 11.8002C0.899983 12.1502 0.749983 12.8502 1.04998 13.3502C1.39998 13.8502 2.09998 14.0002 2.59998 13.7002L3.44998 13.1502V25.8002C3.44998 27.5502 4.84998 28.9502 6.59998 28.9502H25.4C27.15 28.9502 28.55 27.5502 28.55 25.8002V13.1502L29.4 13.7002C29.6 13.8002 29.8 13.9002 30 13.9002C30.35 13.9002 30.75 13.7002 30.95 13.4002C31.3 12.8502 31.15 12.1502 30.6 11.8002ZM13.35 26.7502V18.5002C13.35 18.0002 13.75 17.6002 14.25 17.6002H17.75C18.25 17.6002 18.65 18.0002 18.65 18.5002V26.7502H13.35ZM26.3 25.8002C26.3 26.3002 25.9 26.7002 25.4 26.7002H20.9V18.5002C20.9 16.8002 19.5 15.4002 17.8 15.4002H14.3C12.6 15.4002 11.2 16.8002 11.2 18.5002V26.7502H6.69998C6.19998 26.7502 5.79998 26.3502 5.79998 25.8502V11.7002L15.5 5.4002C15.8 5.2002 16.2 5.2002 16.5 5.4002L26.3 11.7002V25.8002Z'
@@ -48,6 +49,7 @@ export default function ContactInfo() {
                     viewBox='0 0 32 32'
                     fill='none'
                     xmlns='http://www.w3.org/2000/svg'
+                    className='text-blue-700'
                   >
                     <g clip-path='url(#clip0_941_17577)'>
                       <path
@@ -87,6 +89,7 @@ export default function ContactInfo() {
                     viewBox='0 0 32 32'
                     fill='none'
                     xmlns='http://www.w3.org/2000/svg'
+                    className='text-blue-700'
                   >
                     <path
                       d='M28 4.7998H3.99998C2.29998 4.7998 0.849976 6.1998 0.849976 7.9498V24.1498C0.849976 25.8498 2.24998 27.2998 3.99998 27.2998H28C29.7 27.2998 31.15 25.8998 31.15 24.1498V7.8998C31.15 6.1998 29.7 4.7998 28 4.7998ZM28 7.0498C28.05 7.0498 28.1 7.0498 28.15 7.0498L16 14.8498L3.84998 7.0498C3.89998 7.0498 3.94998 7.0498 3.99998 7.0498H28ZM28 24.9498H3.99998C3.49998 24.9498 3.09998 24.5498 3.09998 24.0498V9.2498L14.8 16.7498C15.15 16.9998 15.55 17.0998 15.95 17.0998C16.35 17.0998 16.75 16.9998 17.1 16.7498L28.8 9.2498V24.0998C28.9 24.5998 28.5 24.9498 28 24.9498Z'
@@ -106,7 +109,7 @@ export default function ContactInfo() {
             </div>
           </div>
           <div className='w-full px-4 lg:w-1/2 xl:w-5/12'>
-            <div className='relative p-8 bg-white rounded-lg shadow-lg dark:bg-dark-2 sm:p-12'>
+            <div className='relative p-8 bg-gray-400 rounded-lg shadow-2xl dark:bg-dark-2  sm:p-12'>
               <form>
                 <div className='mb-6'>
                   <input
@@ -139,7 +142,7 @@ export default function ContactInfo() {
                 <div>
                   <button
                     type='submit'
-                    className='w-full p-3 text-white transition border rounded border-primary bg-primary hover:bg-opacity-90'
+                    className='w-full p-3 bg-blue-600 text-white transition border rounded border-primary bg-primary hover:opacity-90'
                   >
                     Send Message
                   </button>
